25a51494c170a2c2a35865bf91e607d7f7f1afeb
NITHINR06 <146012853+NITHINR06@users.noreply.github.com>
2023-12-10 13:42:36 +0530

    Update company.java

--- springcore1/src/main/java/com/springcore1/company.java
+++ springcore1/src/main/java/com/springcore1/company.java
@@ -5,35 +5,48 @@ public class company
 
 {
 
+public company() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 public int companyId;
 
 public String companyAddress;
 
+
+
+public int getCompanyId() {
+	return companyId;
+}
+
+
+
+public void setCompanyId(int companyId) {
+	this.companyId = companyId;
+}
+
+
+
+public String getCompanyAddress() {
+	return companyAddress;
+}
+
+
+
+public void setCompanyAddress(String companyAddress) {
+	this.companyAddress = companyAddress;
+}
+
+
+
 public company(int companyId, String companyAddress) {
-
-super();
-
-this.companyId = companyId;
-
-this.companyAddress = companyAddress;
-
+	super();
+	this.companyId = companyId;
+	this.companyAddress = companyAddress;
 }
 
-public company(int companyId) {
 
-super();
-
-this.companyId = companyId;
-
-}
-
-public company(String companyAddress) {
-
-super();
-
-this.companyAddress = companyAddress;
-
-}
 
 @Override
 
